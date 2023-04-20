94f93019cd10f63554c34331d2dde3fd30737aad
Justin Aquino <justin.aquino25@gmail.com>
2023-04-20 16:18:52 -0700

    rendering disabled button

--- src/Screens/ProductScreen.jsx
+++ src/Screens/ProductScreen.jsx
@@ -1,16 +1,87 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import products from "../products";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../Components/Rating";
 
 const ProductScreen = () => {
   let { id } = useParams();
   const product = products.find((p) => p._id === id);
+  const [isInStock, setIsInStock] = useState(true);
+
+  useEffect(() => {
+    if (product.countInStock > 0) setIsInStock(false); //conditionally render disabled attribute
+  }, []);
 
   console.log(id, product);
   return (
-    <div>
-      <h3>{product.name}</h3>ProductScreen
-    </div>
+    <>
+      <Link
+        to="/"
+        className=" btn btn-primary my-3"
+      >
+        Go Back
+      </Link>
+      <Row className="m-4">
+        <Col md={6}>
+          <Image
+            src={product.image}
+            alt={product.name}
+            fluid
+          />
+        </Col>
+        <Col>
+          <h3>{product.name}</h3>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+          <h3>${product.price}</h3>
+          <p>{product.description}</p>
+        </Col>
+        <Col className="m-auto">
+          <Card
+            className="border-primary"
+            rounded
+          >
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col> Status:</Col>
+                  <Col>
+                    <strong>
+                      {" "}
+                      {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                    </strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price: </Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                    <button
+                      className="btn btn-outline-secondary"
+                      disabled={isInStock}
+                    >
+                      Add to Cart
+                    </button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
   );
 };
 
--- src/products.js
+++ src/products.js
@@ -8,7 +8,7 @@ const products = [
     brand: "Apple",
     category: "Electronics",
     price: 89.99,
-    countInStock: 10,
+    countInStock: 0,
     rating: 3.5,
     numReviews: 12,
   },
